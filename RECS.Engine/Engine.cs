50fa236db74d4c7c973a42f3707e96bd1266a230
Robert Gehrsitz <robertg@digwave.com>
2024-11-01 08:15:40 -0400

    Added schema

--- RECS.Engine/Engine.cs
+++ RECS.Engine/Engine.cs
@@ -1,6 +1,6 @@
 ﻿namespace RECS.Engine
 {
-    public class Class1
+    public class Engine
     {
 
     }
--- RECS.Tests/CompilerTests.cs
+++ RECS.Tests/CompilerTests.cs
@@ -1,6 +1,6 @@
 namespace RECS.Tests
 {
-    public class UnitTest1
+    public class CompilerTests
     {
         [Fact]
         public void Test1()
